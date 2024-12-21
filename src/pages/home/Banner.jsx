06ce4e92240a9148c3c892d3e815220af27e1d53
Ariyan Yeamin <yeaminfoysal14@gmail.com>
2024-12-21 12:45:31 -0800

    primary setup complete

---
+++ src/pages/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center'>This is banner</h1>
+        </div>
+    );
+};
+
+export default Banner;
